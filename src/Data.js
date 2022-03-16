7a551141b672ddd69e4be4433dc3751793375ce8
Gord Bond <gordbondartist@gmail.com>
2022-03-15 22:44:20 -0400

    Names Added.

--- src/Data.js
+++ src/Data.js
@@ -1,34 +1,34 @@
 const data = [{
         id: 1,
-        name: "Island",
+        name: "Brickhead",
         image: "https://amplify-imggenerator-dev-193407-deployment.s3.amazonaws.com/brickhead.jpg"
     }, {
         id: 2,
-        name: "Forest",
+        name: "Tiger",
         image: "https://amplify-imggenerator-dev-193407-deployment.s3.amazonaws.com/tiger.png"
     }, {
         id: 3,
-        name: "Whale",
+        name: "Head just sitting on a rug",
         image: "https://amplify-imggenerator-dev-193407-deployment.s3.amazonaws.com/head_rug_36_48.jpg"
     }, {
         id: 4,
-        name: "Whale",
+        name: "Listen Here",
         image: "https://amplify-imggenerator-dev-193407-deployment.s3.amazonaws.com/listen_here_24_24.jpg"
     }, {
         id: 5,
-        name: "Whale",
+        name: "Noodle Guts",
         image: "https://amplify-imggenerator-dev-193407-deployment.s3.amazonaws.com/noodle_guts_44_52.jpg"
     }, {
         id: 6,
-        name: "Whale",
+        name: "A punch to the face",
         image: "https://amplify-imggenerator-dev-193407-deployment.s3.amazonaws.com/punch_to_the_face_52_44.jpg"
     }, {
         id: 7,
-        name: "Whale",
+        name: "Smoking in the bushes",
         image: "https://amplify-imggenerator-dev-193407-deployment.s3.amazonaws.com/smoking_in_the_bushes_30_40.jpg"
     }, {
         id: 8,
-        name: "Whale",
+        name: "Stubbed Toe",
         image: "https://amplify-imggenerator-dev-193407-deployment.s3.amazonaws.com/stubbed_toe_56_56.jpg"
     },
 ]
